9a9c946f1a420dc7ab9338da12224c9d5aa49667
Breno Lopes do Carmo <brenolopesdocarmo@gmail.com>
2021-06-11 14:32:59 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,38 +5,6 @@ const url = 'https://api.themoviedb.org/3/search/multi?api_key=fa08c4b23ba5324d5
 // elementos DOM
 const buttonElement = document.querySelector('#search__button');
 const inputElement = document.querySelector('#search__input');
-var nmrPagina = 1;
-
-const buttonPrev = document.querySelector('#prev');
-const buttonNext = document.querySelector('#next');
-
-
-    buttonNext.onclick = function(){
-        nmrPagina= nmrPagina + 1;
-        console.log(nmrPagina);
-    };
-    
-    buttonPrev.onclick = function(){
-        nmrPagina= nmrPagina - 1;
-        console.log(nmrPagina);
-    };
-
-
-function verificaPagina(){
-    if(nmrPagina == 1){
-        buttonPrev.classList.replace("prev", "prev hidden");
-    }else{
-        buttonPrev.classList.replace("prev hidden", "prev");
-    }
-}
-
-
-    
-    
-   
-
-
-
 
 function executaPesquisa () {
     let query = inputElement;
@@ -56,21 +24,3 @@ buttonElement.addEventListener ('click', executaPesquisa);
 
 
 
-
-// buttonElement.onclick = function(event){
-//     event.preventDefault();
-//     const value = inputElement.value;
-
-//     const newUrl = url + '&query=' + value;
-
-//     fetch(newUrl)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             console.log('Data: ',data);
-//         })
-//         .catch((error) => {
-//             console.log('Erro: ', error)
-//         });
-
-//     console.log('Value', value);
-// }
