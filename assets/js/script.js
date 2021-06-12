ab2d50b9b8cf4539f95f7f31d77f64b010fa87ba
Breno Lopes do Carmo <brenolopesdocarmo@gmail.com>
2021-06-12 18:47:21 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,5 @@
-// valores iniciais e constantes
 const API_KEY = 'fa08c4b23ba5324d5e306ec5d6eab4d5';
-const url = 'https://api.themoviedb.org/3/search/multi?api_key=fa08c4b23ba5324d5e306ec5d6eab4d5&language=pt-BR&page=1';
-
-// elementos DOM
-const buttonElement = document.querySelector('#search__button');
-const inputElement = document.querySelector('#search__input');
-
-function executaPesquisa () {
-    let query = inputElement;
-    let xhr = new XMLHttpRequest ();
-    xhr.onload = exibeNoticias;
-    xhr.open ('GET', `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=pt-BR&query=${query}&page=${nmrPagina}`);
-    xhr.send ();
-}
-
-buttonElement.addEventListener ('click', executaPesquisa);
+const posterUrl = 'https://image.tmdb.org/t/p/w500/';
 
 
 
