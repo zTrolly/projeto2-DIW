842667b089135ea03de806491b410198db72b410
Breno Lopes do Carmo <61760048+zTrolly@users.noreply.github.com>
2022-06-13 11:18:03 -0300

    a

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,17 +7,23 @@ function toggleMenu(){
 btnMobile.addEventListener('click', toggleMenu);
 
 
+
+
+
+
+
 /*PARTE DA API DE DESTAQUES*/
 const API_KEY = 'fa08c4b23ba5324d5e306ec5d6eab4d5';
-const imgPath = 'https://image.tmdb.org/t/p/w500';
+const imgPath = 'https://image.tmdb.org/t/p/w500/';         // toda imagem vai ter 500 largura
 const trendingPath = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}&language=pt-BR`;
 
-async function exibirTrending(){
-    let text = '';
+async function exibirTrending(){ // espra carregar tudo de dps libera
+    let text = ''; 
     let display =  document.getElementById('display');
-    const d = await fetch(`${trendingPath}`);
+    const d = await fetch(`${trendingPath}`); // espera carregar o treading path
     const f = await d.json();
     console.log(f);
+    
     for(let i =0; i < f.results.length; i++){
         text = text + `
         <div class="serieBox">
